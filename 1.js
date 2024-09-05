5d5799e4dd5e7f6b115efc7106ca7b399219d2c5
ajuteixeira <teixeirajuuh@gmail.com>
2024-09-05 16:20:43 -0300

    questão 1

---
+++ 1.js
@@ -0,0 +1,19 @@
+// 1) Observe o trecho de código abaixo:
+
+// int INDICE = 13, SOMA = 0, K = 0;
+
+// Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+// Imprimir(SOMA);
+
+// Ao final do processamento, qual será o valor da variável SOMA?
+
+let i = 13;
+let k = 0;
+let sum = 0;
+
+while (k < i) {
+  k = k + 1;
+  sum = sum + k;
+}
+
+console.log(`O valor da SOMA é: ${sum}`);
